350ec9d7f4fe112bb54c069e60e4bb596bf8b8e8
Aayush <aayush.142001@gmail.com>
2024-02-20 01:05:39 +0530

    Update

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({ product }) => {
   return (
-    <div className='cursor-pointer flex flex-col 
-    items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3     '>
+    <div
+      className="cursor-pointer flex flex-col 
+    items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border"
+    >
+      <div className="h-[13rem] w-[10rem]">
+        <img
+          className="object-cover object-top w-full h-full"
+          src={product.imageUrl}
+          alt=""
+        />
+      </div>
 
-        <div className='h-[13rem] w-[10rem]'>
-            <img src='https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg'
-            alt=""/>
-
-        </div>
-
-
+      <div className="p-4">
+        <h3 className="text-lg font-medium text-gray-900">{product.brand}</h3>
+        <p className="mt-2 text-sm text-gray-500">
+          {product.title}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCard
+export default HomeSectionCard;
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
+import {mens_kurta} from '../../../Data/mens_kurta.js'
 
 const HomePage = () => {
     return (
         <div>
             <MainCarousel/>
 
-            <div>
-                Other Section
+            <div className='space-y-10  py-20 flex flex-col justify-center px-5 lg:px-10'>
+                <HomeSectionCarousel data={mens_kurta}/>
+                <HomeSectionCarousel data={mens_kurta}/>
+                <HomeSectionCarousel data={mens_kurta}/>
+                <HomeSectionCarousel data={mens_kurta}/>
+                <HomeSectionCarousel data={mens_kurta}/>
+                <HomeSectionCarousel data={mens_kurta}/>
             </div>
         </div>
     )
