b5b43981add4363ec89e0d3fcc0a211f288f9565
Aayush <aayush.142001@gmail.com>
2024-02-20 03:10:35 +0530

    Update

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -4,22 +4,25 @@ import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { Button } from "@mui/material";
 
-const HomeSectionCarousel = ({data}) => {
-    const[activeIndex,setActiveIndex]=useState(0);
+const HomeSectionCarousel = ({ data,sectionName }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const slidePrev=()=>setActiveIndex(activeIndex-1);
-  const slideNext=()=>setActiveIndex(activeIndex+1)
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1)
 
-  const syncActiveIndex=({item})=>setActiveIndex(item)
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
 
-  const items = data.slice(0,10).map((item) => <HomeSectionCard product={item} />);
+  const items = data
+    .slice(0, 15)
+    .map((item) => <HomeSectionCard product={item} />);
   return (
     <div className="border">
+        <h2 className="text-2xl font-extrabold text-gray-800 py-5 " >{sectionName}</h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
@@ -27,44 +30,45 @@ const HomeSectionCarousel = ({data}) => {
           responsive={responsive}
           disableDotsControls
           onSlideChanged={syncActiveIndex}
-          activeIndex={activeIndex} 
+          activeIndex={activeIndex}
         />
-        {activeIndex!==items.length-5 && <Button
-          variant="contained"
-          className="z-50 bg-white"
-          onClick={slideNext}
-          sx={{
-            position: "absolute",
-            right: "0rem",
-            top: "8rem",
-            transform: "translateX(50%) rotate(90deg)",
-            bgcolor: "white",
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowLeftIcon
-            sx={{ transform: "rotate(90deg)", color: "black" }}
-          />
-        </Button>}
+        {activeIndex !== items.length - 5 && <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              right: "0rem",
+              top: "8rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        }
 
-        {activeIndex !==0 && <Button
-        variant="contained"
-        className="z-50 bg-white"
-        onClick={slidePrev}
-        sx={{
-          position: "absolute",
-          left: "0rem",
-          top: "8rem",
-          transform: "translateX(-50%) rotate(-90deg)",
-          bgcolor: "white",
-        }}
-        aria-label="next"
-      >
-        <KeyboardArrowLeftIcon
-          sx={{ transform: "rotate(90deg)", color: "black" }}
-        />
-      </Button> }
-
+        {activeIndex !== 0 && <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              left: "0rem",
+              top: "8rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        }
       </div>
     </div>
   );
