7eabbb582a33c2795b9a3b0abbe1ed87efbde0cd
Aayush <aayush.142001@gmail.com>
2024-03-18 14:40:11 +0530

    update

--- src/State/store.js
+++ src/State/store.js
@@ -1,8 +1,13 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
-
 import { authReducer } from "./Auth/Reducer";
+import { cartReducer } from "./Cart/Reducer";
+import { customerProductReducer } from "./Product/Reducer";
+
 const rootReducers=combineReducers({
-    auth:authReducer
+    auth:authReducer,
+    product:customerProductReducer,
+    cart:cartReducer,
+
 })
 export const store = legacy_createStore(rootReducers,applyMiddleware(thunk))
