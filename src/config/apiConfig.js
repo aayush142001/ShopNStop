5f515897a5c441193407d077e17ec22d4cccb16a
Aayush <aayush.142001@gmail.com>
2024-06-07 14:13:17 +0530

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5454"
+export const API_BASE_URL = "https://shopnstopbackend-production-5363.up.railway.app/"
 
 const jwt=localStorage.getItem("jwt")
 
